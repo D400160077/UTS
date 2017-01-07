faa7294d148b16542764a0d889b11056a9317682
D400160077 <jidilmustofa24@gmail.com>
2017-01-07 20:11:57 +0700

    No 5

---
+++ Soal UTS no 5.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int a=0;
+int b=0;
+int c=0;
+float D=0;
+fload x1, x2;
+
+int main() {}
+
+    printf("Masukan Variabel a, b, dan c\n");
+    scanf("%d %d %d",&a &b &c);
+
+    if(a==0} {
+
+        D = (b*b) - (4*a*c);
+        x1 = (-b + aqrt (D) )/(2*a);
+        x2 = (b + aqrt (D) )/(2*a);
+
+        printf ("x1= %f, x2 %f \n", x1, x2);
+    }
+    return 0;
